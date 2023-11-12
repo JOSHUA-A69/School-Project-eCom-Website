5778baaa18f0a5793d207267356e7c6de198149b
Joshua Russel Uy <116649829+JOSHUA-A69@users.noreply.github.com>
2023-11-12 16:17:42 +0800

    Add files via upload

--- events.js
+++ events.js
@@ -1,3 +1,25 @@
+function handleHeartButtonClick(buttonClass) {
+    var buttons = document.querySelectorAll('.' + buttonClass);
+
+    buttons.forEach(function(button) {
+        var isFavorite = false;
+
+        button.addEventListener('click', function() {
+            isFavorite = !isFavorite; // Toggle between true and false
+
+            if (isFavorite) {
+                alert('Added to Favorites');
+                button.classList.add('redHeart'); // Add a class to turn the heart red
+            } else {
+                alert('Removed from Favorites');
+                button.classList.remove('redHeart'); // Remove the class to revert to the default color
+            }
+        });
+    });
+}
+
+handleHeartButtonClick('heartButton');
+
 function handleButtonClick(buttonClass, message) {
     var buttons = document.querySelectorAll('.' + buttonClass);
 
@@ -8,5 +30,5 @@ function handleButtonClick(buttonClass, message) {
     });
 }
 
-handleButtonClick('heartButton', 'Added to Favorites');
 handleButtonClick('addtoCart', 'Added to Cart');
+
