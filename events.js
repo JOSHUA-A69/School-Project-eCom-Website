087bb742b9481544a105509f91a20ee3ff1528d4
Joshua Russel Uy <116649829+JOSHUA-A69@users.noreply.github.com>
2023-11-23 12:05:25 +0800

    Update events.js

--- events.js
+++ events.js
@@ -65,39 +65,4 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
 
-  document.addEventListener('DOMContentLoaded', function () {
-    var backgroundContainer = document.getElementById('changeImage');
-    var imageIndex = 0;
-    var imageList = [
-        'https://i.postimg.cc/xdpCq2Cf/USAF-photographer.jpg',
-        'https://i.postimg.cc/ZRPqkJYV/2017-04-07-09-59-14.jpg',
-        'https://i.postimg.cc/bYkyfbQN/gv-Q9-Nh-QP8wbb-M32j-Xy4-V3j.jpg',
-        'https://i.postimg.cc/8C4KqTHb/female-photographer-taking-photo.jpg',
-        'https://i.postimg.cc/GpdMdQbr/2017-08-30-07-11-09.jpg',
-    ];
-
-    function changeBackground() {
-        // Fade out
-        backgroundContainer.classList.add('fade-out');
-
-        // Change the background image
-        backgroundContainer.style.backgroundImage = 'url(' + imageList[imageIndex] + ')';
-
-        // Fade in
-        setTimeout(function () {
-            backgroundContainer.classList.remove('fade-out');
-            backgroundContainer.classList.add('fade-in');
-        }, 100);
-
-        // Reset classes for the next transition
-        setTimeout(function () {
-            backgroundContainer.classList.remove('fade-in');
-        }, 2000);
-
-        // Increment the index for the next image
-        imageIndex = (imageIndex + 1) % imageList.length;
-    }
-
-    // Change the background every 5 seconds (5000 milliseconds)
-    setInterval(changeBackground, 5000);
-});
+    
